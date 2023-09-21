2f8aa7cb125de79c96e0cc6401615d392eb05ded
Eugene Cross <eugene.cross@gmail.com>
2023-09-21 20:40:27 +0100

    Initial Commit

---
+++ FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by Eugene on 21/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
