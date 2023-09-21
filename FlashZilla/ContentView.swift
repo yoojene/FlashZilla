d3a1b76dd360a8dface89b9edc0ecb6ae54e758f
Eugene Cross <eugene.cross@gmail.com>
2023-09-21 21:27:06 +0100

    Day 86 pt 2a UINotificationFeedbackGenerator haptics

--- FlashZilla/ContentView.swift
+++ FlashZilla/ContentView.swift
@@ -8,41 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var offset = CGSize.zero
-    @State private var isDragging = false
    
     var body: some View {
-        
-        let dragGesture = DragGesture()
-            .onChanged { value in
-                offset = value.translation
-            }
-            .onEnded { _ in
-                withAnimation {
-                    offset = .zero
-                    isDragging = false
-                }
-            }
-
-        let pressedGesture = LongPressGesture()
-            .onEnded { value in
-                withAnimation {
-                    isDragging = true
-                }
-            }
-        
-        let combined = pressedGesture.sequenced(before: dragGesture)
-        
-        
-        Circle()
-            .fill(.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combined)
-        
-        
+        Text("Hello world")
+            .padding()
+            .onTapGesture(perform: simpleSuccess)
+    }
+    
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
     }
 
 }
