18f90f15d6ca2a5fbbd53535f5a675ab26e9bde2
Eugene Cross <eugene.cross@gmail.com>
2023-09-21 21:51:24 +0100

    Day 86 pt 3c contentShape() again

--- FlashZilla/ContentView.swift
+++ FlashZilla/ContentView.swift
@@ -11,20 +11,14 @@ import SwiftUI
 struct ContentView: View {
    
     var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(.blue)
-                .frame(width: 300, height: 300)
-                .onTapGesture {
-                    print("Rectangle tapped!")
-                }
-            Circle()
-                .fill(.red)
-                .frame(width: 300, height: 300)
-                .contentShape(Rectangle()) // the tap gesture will work on the rectangle too
-                .onTapGesture {
-                    print("Circle tapped!")
-                }
+        VStack {
+            Text("Hello")
+            Spacer().frame(height: 100)
+            Text("world")
+        }
+        .contentShape(Rectangle())
+        .onTapGesture {
+            print("Vstack tapped")
         }
     }
   
