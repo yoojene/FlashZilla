9be62d6b7c89846fbc07e2646348e25d9d842b38
Eugene Cross <eugene.cross@gmail.com>
2023-09-21 21:41:30 +0100

    Day 86 pt 2b Core Haptics

--- FlashZilla/ContentView.swift
+++ FlashZilla/ContentView.swift
@@ -5,14 +5,17 @@
 //  Created by Eugene on 21/09/2023.
 //
 
+import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var engine: CHHapticEngine?
    
     var body: some View {
         Text("Hello world")
-            .padding()
-            .onTapGesture(perform: simpleSuccess)
+            .onAppear(perform: prepareHaptics)
+            .onTapGesture(perform: complexSuccess)
     }
     
     func simpleSuccess() {
@@ -20,6 +23,36 @@ struct ContentView: View {
         generator.notificationOccurred(.success)
     }
 
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+
+        var events = [CHHapticEvent]()
+        
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        
+        events.append(event)
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern \(error.localizedDescription)")
+        }
+        
+    }
 }
     
 
