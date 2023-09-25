6ed6dfd94e1e5f80ffb8869c4ce774d3c4b87589
Eugene Cross <eugene.cross@gmail.com>
2023-09-25 10:43:31 +0100

    Day 87, accessibilityReduceTransparency

--- FlashZilla/ContentView.swift
+++ FlashZilla/ContentView.swift
@@ -7,30 +7,18 @@
 
 import SwiftUI
 
-
-// wrapper on UIKit UIAccessibility makes the code simpler
-func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
-    if UIAccessibility.isReduceMotionEnabled {
-        return try body()
-    } else {
-        return try withAnimation(animation, body)
-    }
-}
-
 struct ContentView: View {
     
-    @Environment(\.accessibilityReduceMotion) var reduceMotion
-    @State private var scale = 1.0
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
 
     var body: some View {
        
         Text("Success")
-            .scaleEffect(scale)
-            .onTapGesture {
-                withOptionalAnimation {
-                    scale *= 1.5
-                }
-            }
+            .padding()
+            .background(reduceTransparency ? .black : .black.opacity(0.5))
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+            
         }
   
 }
