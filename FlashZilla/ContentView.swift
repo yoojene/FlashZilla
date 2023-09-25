08863b957b5178dce3148e8ed69d7ac9f4190551
Eugene Cross <eugene.cross@gmail.com>
2023-09-25 10:17:00 +0100

    Day 87, timer events

--- FlashZilla/ContentView.swift
+++ FlashZilla/ContentView.swift
@@ -5,21 +5,26 @@
 //  Created by Eugene on 21/09/2023.
 //
 
-import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
+    
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
+    
+    @State private var counter = 0
+    
    
     var body: some View {
-        VStack {
-            Text("Hello")
-            Spacer().frame(height: 100)
-            Text("world")
-        }
-        .contentShape(Rectangle())
-        .onTapGesture {
-            print("Vstack tapped")
-        }
+        Text("Hello world")
+            .onReceive(timer) { time in
+                if counter == 5 {
+                    timer.upstream.connect().cancel()
+                } else {
+                    print("The time is now \(time)")
+                    
+                }
+                counter += 1
+            }
     }
   
 }
