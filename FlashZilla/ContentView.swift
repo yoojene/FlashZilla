ab8363f44013e7b8e90c330302b2e9338c50ca1a
Eugene Cross <eugene.cross@gmail.com>
2023-09-25 11:55:08 +0100

    Day 88: Counting down with a timer

--- FlashZilla/ContentView.swift
+++ FlashZilla/ContentView.swift
@@ -19,6 +19,12 @@ struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     @State private var cards = Array<Card>(repeating: Card.example, count: 10)
 
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @Environment(\.scenePhase) var scenePhase
+    @State private var isActive = true
+    
     var body: some View {
         
         ZStack {
@@ -28,6 +34,15 @@ struct ContentView: View {
                 .ignoresSafeArea()
             
             VStack {
+                
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(Capsule())
+                    
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: cards[index]) {
@@ -62,7 +77,20 @@ struct ContentView: View {
                 .padding()
             }
         }
-        
+        .onReceive(timer) { time in
+            guard isActive else { return }
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
+        .onChange(of: scenePhase) { newPhase in
+            if newPhase == .active {
+                isActive = true
+            } else {
+                isActive = false
+            }
+            
+        }
      }
     
     func removeCard(at index: Int) {
